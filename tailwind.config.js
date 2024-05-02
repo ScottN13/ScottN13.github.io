59c7878f31009f43ec8721c931e88db475bd581c
scotty <steluta.cotiu@gmail.com>
2024-05-02 15:27:39 +0300

    massive revamp #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,5 +37,6 @@ module.exports = {
       utils: true, // adds responsive and modifier utility classes
       prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
       logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-      themeRoot: ":root", // The element that receives theme color CSS variables
+      themeRoot: ":root", // The element that receives theme color CSS variables  
+  }
 }

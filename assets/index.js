5bc3df09189324983e6ce126caf485b9001eab1a
scotty <steluta.cotiu@gmail.com>
2024-07-30 20:13:50 +0300

    e

--- assets/index.js
+++ assets/index.js
@@ -18,9 +18,10 @@ function newsDropdown() {
     }
   }
 
-
+/*
 webamp.setTracksToPlay([
   {url: 'https://www.youtube.com/watch?v=e0T0rI-GiR4&list=PL7IgabZ8nkx39Pl2SEMXTGfMnF8PwIYQo&index=17'},
   {url: 'https://www.youtube.com/watch?v=OFP3lEHOpA8&list=PL7IgabZ8nkx39Pl2SEMXTGfMnF8PwIYQo&index=55'},
   {url: 'https://www.youtube.com/watch?v=TN25ghkfgQA&list=PL7IgabZ8nkx39Pl2SEMXTGfMnF8PwIYQo&index=16'}
 ]);
+*/

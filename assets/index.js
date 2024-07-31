fee55b2d5657258721e17d4cc7fe2a5877ec61fa
scotty <steluta.cotiu@gmail.com>
2024-07-31 13:42:58 +0300

    Switched from JS to TS and fixed embed title.

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,4 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function newsDropdown() {
-    document.getElementById("news").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('dropdownbtn')) {
-      var dropdowns = document.getElementById("news-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
+// Webamp attempts.
 /*
 webamp.setTracksToPlay([
   {url: 'https://www.youtube.com/watch?v=e0T0rI-GiR4&list=PL7IgabZ8nkx39Pl2SEMXTGfMnF8PwIYQo&index=17'},
